c496d6f679bd301eeff20e160d7d0acc4746484f
Stefano Amorelli <stefano@amorelli.tech>
2025-05-04 23:17:46 +0300

    refactor(build-package): simplify conda build and remove convert

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,20 +18,10 @@ check_if_meta_yaml_file_exists() {
 
 build_package(){
     conda config --set anaconda_upload yes
-    conda build -c conda-forge -c bioconda --output-folder . .
-    shopt -s nullglob
-    files=(linux-64/*.tar.bz2)
-    [ ${#files[@]} -gt 0 ] && conda convert -p osx-64 "${files[@]}"
+    conda build -c conda-forge -c bioconda
 }
 
-upload_package(){
-    export ANACONDA_API_TOKEN=$INPUT_ANACONDATOKEN
-    for f in linux-64/*.tar.bz2 osx-64/*.tar.bz2 noarch/*.conda; do
-        [ -f "$f" ] && anaconda upload --label main "$f"
-    done
-}
 
 go_to_build_dir
 check_if_meta_yaml_file_exists
 build_package
-upload_package
